78a988cb0b03efba1d274bb7195e4abdf99b05f3
samrithmengly9 <103111739+samrithmengly9@users.noreply.github.com>
2024-06-30 04:45:11 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/transferCoin.ts
@@ -0,0 +1,29 @@
+import wallet from "./wallet";
+
+type TransferCoin = {
+  success: boolean;
+  message: string;
+};
+
+/*
+ * Transfer coin to address. This is native token ie ETH
+ * @param {string} address - The address to transfer to
+ */
+export default async function transferCoin(address: string): Promise<TransferCoin> {
+  try {
+    const transaction = await wallet.sendTransaction({
+      to: address,
+      value: process.env.VALUE as string,
+    });
+    return {
+      success: true,
+      message: transaction.hash,
+    };
+  } catch (error) {
+    console.error(error)
+    return {
+      success: false,
+      message: "Unable to Send Transaction",
+    };
+  }
+}
